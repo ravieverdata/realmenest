52f9035b66f97913bbc71b28a5aab34d796e9c54
Ravi Sharma <ravi@everdata.com>
2023-11-06 18:41:42 +0530

    Client Notes and feedbacks

--- src/controllers/microservice/client/client.controller.ts
+++ src/controllers/microservice/client/client.controller.ts
@@ -235,4 +235,97 @@ export class ClientController {
 
     }
 
+
+
+    // particular client notes 
+    @ApiUnauthorizedResponse({ status: 401, description: "Unauthorized Admin" })
+	@ApiOperation({ summary: "Client Notes" })
+	@ApiResponse({ status: 200, description: "Api success" })
+	@ApiResponse({ status: 422, description: "Bad Request or API error message" })
+	@ApiResponse({ status: 404, description: "Not found!" })
+	@ApiResponse({ status: 500, description: "Internal server error!" })
+    @ApiResponse({ status: 400, description: "Una" })
+    @Get('clientnotes/:id')
+    async clientnotes(@Param('id', ParseIntPipe) id: number,  @Req() request: Request, @Ip() ip: string): Promise<any> {
+
+        if (isNaN(id)) {
+            // Return a BadRequestException with a custom error message for invalid 'id'
+            throw new BadRequestException('Invalid ID. Please provide a valid number.');
+        }
+
+        const microhost = request['microhost'];
+
+        try {
+
+            const headers = {
+                Authorization: `Bearer ${request['accesstoken']}`,
+                'X-User-IP': ip,
+            };
+
+            const response = await axios.get(`${microhost}/client/clientnotes/${id}`, {headers});
+            // Handle the successful response
+            return response.data;
+
+        } catch (error) {
+
+            // Handle the error using the custom error-handling function
+
+            if (error.response) {
+                // The error has a response, indicating it's an HTTP error response from the microservice
+                const errorResponse = error.response;
+                throw errorResponse.data; // Re-throw the error to propagate it to the caller
+            } else {
+                console.error(error.message);
+                throw { message: 'An error occurred while communicating with the server.' }; // Re-throw a custom error
+            }
+        }
+
+    }
+
+
+    // particular client feedbacks 
+    @ApiUnauthorizedResponse({ status: 401, description: "Unauthorized Admin" })
+	@ApiOperation({ summary: "Client Feedbacks" })
+	@ApiResponse({ status: 200, description: "Api success" })
+	@ApiResponse({ status: 422, description: "Bad Request or API error message" })
+	@ApiResponse({ status: 404, description: "Not found!" })
+	@ApiResponse({ status: 500, description: "Internal server error!" })
+    @ApiResponse({ status: 400, description: "Una" })
+    @Get('clientfeedbacks/:id')
+    async clientfeedbacks(@Param('id', ParseIntPipe) id: number,  @Req() request: Request, @Ip() ip: string): Promise<any> {
+
+        if (isNaN(id)) {
+            // Return a BadRequestException with a custom error message for invalid 'id'
+            throw new BadRequestException('Invalid ID. Please provide a valid number.');
+        }
+
+        const microhost = request['microhost'];
+
+        try {
+
+            const headers = {
+                Authorization: `Bearer ${request['accesstoken']}`,
+                'X-User-IP': ip,
+            };
+
+            const response = await axios.get(`${microhost}/client/clientfeedbacks/${id}`, {headers});
+            // Handle the successful response
+            return response.data;
+
+        } catch (error) {
+
+            // Handle the error using the custom error-handling function
+
+            if (error.response) {
+                // The error has a response, indicating it's an HTTP error response from the microservice
+                const errorResponse = error.response;
+                throw errorResponse.data; // Re-throw the error to propagate it to the caller
+            } else {
+                console.error(error.message);
+                throw { message: 'An error occurred while communicating with the server.' }; // Re-throw a custom error
+            }
+        }
+
+    }
+
 }
